757194fc0b5e49994de1a595c4daf4d3a24821ee
Ben Nortier <bjnortier@gmail.com>
2016-09-19 17:03:00 +0200

    print response code

--- post_dht22.py
+++ post_dht22.py
@@ -28,7 +28,8 @@ while True:
                     'humidity': humidity
                 }
             })
-        print(response.content)
+        print('Status: {}'.format(response.status_code))
+        print('Content: {}'.format(response.content))
     except:
         print('Unexpected error:', sys.exc_info()[0])
 

9f743efcfdb8c4dee269e78c5ed0a49348180c2a
Ben Nortier <bjnortier@gmail.com>
2016-09-19 16:15:05 +0200

    DHT22 example

---
+++ post_dht22.py
@@ -0,0 +1,35 @@
+import time
+import requests
+import sys
+import Adafruit_DHT
+
+sensor = Adafruit_DHT.DHT22
+pin = 7
+
+URL = 'https://www.senio.io/publish/rpi_demo'
+UUID = '48fd7f79-66e9-42ff-b437-9759d38a8f04'
+
+while True:
+    humidity, temp = Adafruit_DHT.read_retry(sensor, pin)
+    if humidity is None:
+        continue
+    print('Temp: {} Humidity: {}'.format(temp, humidity))
+
+    try:
+        response = requests.post(
+            URL,
+            headers={
+                'Authorization': 'Bearer {}'.format(UUID)
+            },
+            json={
+                'timestamp': time.time()*1000,
+                'data': {
+                    'temp': temp,
+                    'humidity': humidity
+                }
+            })
+        print(response.content)
+    except:
+        print('Unexpected error:', sys.exc_info()[0])
+
+    time.sleep(60)

c36f8aa05ea9eb6086781ba7b15ff1bc90a576e4
Ben Nortier <bjnortier@gmail.com>
2016-09-20 16:54:48 +0200

    dummy dht22

--- post_dummy_dht22.py
+++ post_dummy_dht22.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python3
 
-import picamera
 import time
 import requests
-import json
 import sys
 
 from senio_config import SenioConfig
 config = SenioConfig()
 
+
 while True:
-    camera = picamera.PiCamera()
-    camera.vflip = True
-    camera.capture('img720x480.jpg')
-    camera.close()
+    humidity, temp = 50.0, 22.0
+    if humidity is None:
+        continue
+    print('Temp: {} Humidity: {}'.format(temp, humidity))
 
     try:
         response = requests.post(
@@ -21,15 +20,16 @@ while True:
             headers={
                 'Authorization': 'Bearer {}'.format(config.key)
             },
-            files={'cam1': open('img720x480.jpg', 'rb')},
-            data={
-                'json': json.dumps({
-                    'timestamp': time.time()*1000
-                })
+            json={
+                'timestamp': time.time()*1000,
+                'data': {
+                    'temp': temp,
+                    'humidity': humidity
+                }
             })
         print('Status: {}'.format(response.status_code))
         print('Content: {}'.format(response.content))
     except:
         print('Unexpected error:', sys.exc_info()[0])
 
-    time.sleep(600)
+    time.sleep(60)

4c6ee2bb7bb02c6919b29a066dd429954b087614
Ben Nortier <bjnortier@gmail.com>
2016-09-20 16:56:07 +0200

    Creating readme

--- post_dummy_dht22.py
+++ post_dummy_dht22.py
@@ -7,29 +7,23 @@ import sys
 from senio_config import SenioConfig
 config = SenioConfig()
 
+humidity, temp = 50.0, 22.0
+print('Temp: {} Humidity: {}'.format(temp, humidity))
 
-while True:
-    humidity, temp = 50.0, 22.0
-    if humidity is None:
-        continue
-    print('Temp: {} Humidity: {}'.format(temp, humidity))
-
-    try:
-        response = requests.post(
-            config.url,
-            headers={
-                'Authorization': 'Bearer {}'.format(config.key)
-            },
-            json={
-                'timestamp': time.time()*1000,
-                'data': {
-                    'temp': temp,
-                    'humidity': humidity
-                }
-            })
-        print('Status: {}'.format(response.status_code))
-        print('Content: {}'.format(response.content))
-    except:
-        print('Unexpected error:', sys.exc_info()[0])
-
-    time.sleep(60)
+try:
+    response = requests.post(
+        config.url,
+        headers={
+            'Authorization': 'Bearer {}'.format(config.key)
+        },
+        json={
+            'timestamp': time.time()*1000,
+            'data': {
+                'temp': temp,
+                'humidity': humidity
+            }
+        })
+    print('Status: {}'.format(response.status_code))
+    print('Content: {}'.format(response.content))
+except:
+    print('Unexpected error:', sys.exc_info()[0])

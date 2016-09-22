d3cdca38603e9ac3b1f85187fd5a93b928770d04
Ben Nortier <bjnortier@gmail.com>
2016-09-22 13:10:19 +0200

    remove /publish/ from base_url

--- senio_config.py
+++ senio_config.py
@@ -5,12 +5,12 @@ class SenioConfig:
     def __init__(self):
         config = configparser.ConfigParser()
         config.read('senio.ini')
-        default_url = 'https://www.senio.io/publish/'
+        default_url = 'https://www.senio.io'
         if 'base_url' in config['sensor']:
             base_url = config['sensor']['base_url']
         else:
             base_url = default_url
-        url = '{}{}'.format(base_url, config['sensor']['name'])
+        url = '{}/publish/{}'.format(base_url, config['sensor']['name'])
         key = config['sensor']['key']
         print('----- CONFIG -----')
         print('url: {}'.format(url))
